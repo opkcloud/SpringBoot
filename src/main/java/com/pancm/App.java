75f8755a19fb5d4ca480ab51af9c7cfbf493e1a0
opkcloud <3024371557@qq.com>
2019-06-12 22:19:21 +0800

    SpringBoot架构代码初次提交...

---
+++ src/main/java/com/pancm/App.java
@@ -0,0 +1,21 @@
+package com.pancm;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+		SpringApplication.run(App.class, args);
+		System.out.println("程序正在运行...");
+    }
+    
+}
---
+++ src/main/java/com/pancm/web/UserRestController.java
@@ -0,0 +1,65 @@
+package com.pancm.web;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.pancm.pojo.User;
+import com.pancm.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * 
+* Title: UserRestController
+* Description: 
+* 用户控制层
+* Version:1.0.0  
+* @author pancm
+* @date 2018年3月19日
+* url: http://localhost:8083/swagger-ui.html#/
+ */
+@RestController
+@RequestMapping(value = "/api")
+public class UserRestController {
+	@Autowired
+    private UserService userService;
+ 
+	@ApiOperation(value = "新增用户信息", httpMethod = "POST", notes = "新增用户信息")
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public boolean addUser(@RequestBody User user) {
+    	System.out.println("开始新增...");
+        return userService.addUser(user);
+    }
+    
+	@ApiOperation(value = "更新用户信息", httpMethod = "PUT", notes = "更新用户信息")
+	@RequestMapping(value = "/updateUser", method = RequestMethod.PUT)
+    public boolean updateUser(@RequestBody User user) {
+    	System.out.println("开始更新...");
+        return userService.updateUser(user);
+    }
+	
+	@ApiOperation(value = "根据用户Id删除用户信息", httpMethod = "DELETE", notes = "根据用户Id删除用户信息")
+	@RequestMapping(value = "/deleteUser", method = RequestMethod.DELETE)
+    public boolean deleteUser(@RequestParam(value = "userId", required = true) int userId) {
+    	System.out.println("开始删除...");
+        return userService.deleteUser(userId);
+    }
+	
+	@ApiOperation(value = "根据名字查询用户信息", httpMethod = "GET", notes = "根据名字查询用户信息")
+    @RequestMapping(value = "/findByUserName", method = RequestMethod.GET)
+    public User findByUserName(@RequestParam(value = "userName", required = true) String userName) {
+    	System.out.println("开始查询...");
+        return userService.findUserByName(userName);
+    }
+    
+	@ApiOperation(value = "查询所有用户信息", httpMethod = "GET", notes = "查询所有用户信息")
+    @RequestMapping(value = "/userAll", method = RequestMethod.GET)
+    public List<User> findByUserAge() {
+    	System.out.println("开始查询所有数据...");
+        return userService.findAll();
+    }
+}
