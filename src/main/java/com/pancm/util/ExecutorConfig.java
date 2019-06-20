3f207dc70f08154906d49d484513ca31179d0d08
opkcloud <3024371557@qq.com>
2019-06-20 16:54:38 +0800

    添加线程池

---
+++ src/main/java/com/pancm/util/ExecutorConfig.java
@@ -0,0 +1,39 @@
+package com.pancm.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+public class ExecutorConfig {
+
+    /**
+     * Springboot线程池的使用和扩展
+     * url: https://mp.weixin.qq.com/s/SCbZueYmODmCXUhn6y3q5A
+     */
+
+    private static final Logger logger = LoggerFactory.getLogger(ExecutorConfig.class);
+
+    @Bean
+    public Executor asyncServiceExecutor() {
+        logger.info("start asyncServiceExecutor");
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(9999);
+        executor.setThreadNamePrefix("async-service-");
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+
+}
