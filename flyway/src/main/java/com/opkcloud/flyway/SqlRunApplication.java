0427d3c56a00d59857200923852d1b244fd2db53
ningjie <ningjie@edsz8.com>
2020-11-06 14:01:23 +0800

    add flyway module

---
+++ flyway/src/main/java/com/opkcloud/flyway/SqlRunApplication.java
@@ -0,0 +1,13 @@
+package com.opkcloud.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlRunApplication.class, args);
+    }
+
+}
